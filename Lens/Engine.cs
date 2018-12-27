02f1b3d21a66aea887217c64ab6c292aeb4f62ae
Egor Dorichev <egordorichev@gmail.com>
2018-12-27 12:42:11 +0300

    Initial commit, basic app running

---
+++ Lens/Engine.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lens {
+	public class Game : Microsoft.Xna.Framework.Game {
+		public GraphicsDeviceManager graphicsDevice;
+		public SpriteBatch spriteBatch;
+
+		public Game() {
+			graphicsDevice = new GraphicsDeviceManager(this) {
+				PreferMultiSampling = true
+			};
+		}
+
+		protected override void Initialize() {
+			base.Initialize();
+		}
+
+		protected override void LoadContent() {
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void UnloadContent() {
+		}
+
+		protected override void Update(GameTime gameTime) {
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+			    ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
+				    Keys.Escape)) {
+				Exit();
+			}
+
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.Black);
+
+			spriteBatch.Begin();
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ Lens/Launcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Lens {
+	public static class Launcher {
+		[STAThread]
+		static void Main() {
+			using (var game = new Game()) {
+				game.Run();
+			}
+		}
+	}
+}
