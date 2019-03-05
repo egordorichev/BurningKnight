85df56d084bf4e9a56496f07f7ba9a037d98d585
Egor Dorichev <egordorichev@gmail.com>
2019-03-05 19:40:52 +0300

    Working on ui

--- BurningKnight/entity/ui/UiEntity.cs
+++ BurningKnight/entity/ui/UiEntity.cs
@@ -4,6 +4,8 @@ namespace BurningKnight.entity.ui {
 	public class UiEntity : Entity {
 		public UiEntity() {
 			Depth = Layers.Ui;
+			AlwaysActive = true;
+			AlwaysVisible = true;
 		}
 	}
 }
--- BurningKnight/entity/ui/UiInventory.cs
+++ BurningKnight/entity/ui/UiInventory.cs
@@ -1,11 +1,35 @@
 using BurningKnight.entity.creature.player;
+using Lens.assets;
+using Lens.graphics;
 
 namespace BurningKnight.entity.ui {
 	public class UiInventory : UiEntity {
+		private TextureRegion itemSlot;
+		private TextureRegion bomb;
+		private TextureRegion key;
+		private TextureRegion coin;
+		
+		private TextureRegion heart;
+		private TextureRegion halfHeart;
+		private TextureRegion heartBackground;
+		private TextureRegion changedHeartBackground;
+		
 		private Player player;
 		
 		public UiInventory(Player player) {
 			this.player = player;
+
+			var anim = Animations.Get("ui");
+
+			itemSlot = anim.GetSlice("item_slot");
+			bomb = anim.GetSlice("bomb");
+			key = anim.GetSlice("key");
+			coin = anim.GetSlice("coin");
+			
+			heart = anim.GetSlice("heart");
+			halfHeart = anim.GetSlice("half_heart");
+			heartBackground = anim.GetSlice("heart_bg");
+			changedHeartBackground = anim.GetSlice("heart_hurt_bg");
 		}
 
 		public override void Render() {
