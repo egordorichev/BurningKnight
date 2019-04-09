d0f8d453d0a28314fd90002e80aa6a662575696a
Egor Dorichev <egordorichev@gmail.com>
2019-04-09 17:10:34 +0300

    Fake loading screen

--- BurningKnight/state/LoadState.cs
+++ BurningKnight/state/LoadState.cs
@@ -1,17 +1,23 @@
+using System;
 using System.Threading;
+using BurningKnight.assets;
 using BurningKnight.assets.lighting;
-using BurningKnight.entity.level;
 using BurningKnight.entity.level.tile;
 using BurningKnight.physics;
 using BurningKnight.save;
 using Lens;
 using Lens.entity;
 using Lens.game;
+using Lens.graphics;
+using Microsoft.Xna.Framework;
 
 namespace BurningKnight.state {
 	public class LoadState : GameState {
 		public string Path;
 		private Area gameArea;
+		private bool ready;
+		private bool down;
+		private float alpha;
 		
 		public override void Init() {
 			base.Init();
@@ -31,12 +37,41 @@ namespace BurningKnight.state {
 
 				SaveManager.Load(gameArea, SaveType.Level, Path);
 				SaveManager.Load(gameArea, SaveType.Player, Path);
-				
-				Engine.Instance.SetState(new InGameState(gameArea));
+
+				ready = true;
 			});
 
 			thread.Priority = ThreadPriority.Lowest;
 			thread.Start();
+		}
+
+		public override void Update(float dt) {
+			base.Update(dt);
+
+			if (down) {
+				if (ready && ((Engine.Version.Debug) || Time > 3f)) {
+					alpha -= dt * 5;
+				}
+			} else {
+				alpha = Math.Min(1, alpha + dt * 5);
+
+				if (alpha >= 0.95f) {
+					alpha = 1;
+					down = true;
+				}
+			}
+
+			if (ready && ((down && alpha < 0.05f) || (Engine.Version.Debug))) {
+				Engine.Instance.SetState(new InGameState(gameArea));
+			}
+		}
+
+		public override void RenderUi() {
+			base.RenderUi();
+			
+			Graphics.Color = new Color(1f, 1f, 1f, alpha);
+			Graphics.Print($"Coming soon tm {Math.Min(102, Math.Floor(Time / 3f * 100f))}%", Font.Medium, new Vector2(4, 4));
+			Graphics.Color = ColorUtils.WhiteColor;
 		}
 	}
 }
