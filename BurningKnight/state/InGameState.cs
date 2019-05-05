b0e4d97af123e7bd24e354dcf25e3d26e0d48fd9
Egor <egordorichev@gmail.com>
2019-05-05 14:54:46 +0300

    Implemented icons and exe naming

--- BurningKnight/state/InGameState.cs
+++ BurningKnight/state/InGameState.cs
@@ -465,10 +465,10 @@ namespace BurningKnight.state {
 
 			gameOverMenu.Setup();
 
-			Ui.Add(new UiString(Font.Medium) {
+			/*Ui.Add(new UiString(Font.Medium) {
 				Label = "[cl blue]^^Awesome^^, [dl]this[cl] [sp 2]_seems_\n[sp 0.5]_to work_[sp] now!!\n[cl red][ev test]##SOO COOL!!!##",
 				Position = new Vector2(32, 32)
-			});
+			});*/
 		}
 
 		public bool HandleEvent(Event e) {
