013a99a9ceac060dc3cf4064f6a65b66b0dc9b86
Egor Dorichev <egordorichev@gmail.com>
2019-09-21 17:03:02 +0300

    Npc save room layout

--- BurningKnight/level/rooms/special/NpcSaveRoom.cs
+++ BurningKnight/level/rooms/special/NpcSaveRoom.cs
@@ -1,11 +1,15 @@
+using System.Linq;
 using BurningKnight.entity.creature.npc;
+using BurningKnight.entity.door;
+using BurningKnight.level.tile;
 using BurningKnight.save;
+using Lens.util.math;
 using Microsoft.Xna.Framework;
 
 namespace BurningKnight.level.rooms.special {
 	public class NpcSaveRoom : SpecialRoom {
 		public override int GetMaxConnections(Connection Side) {
-			return Side == Connection.All ? 1 : 0;
+			return 1;
 		}
 
 		public override int GetMinConnections(Connection Side) {
@@ -16,8 +20,18 @@ namespace BurningKnight.level.rooms.special {
 			return base.CanConnect(R) && !(R is NpcKeyRoom);
 		}
 
+		public override int GetMaxWidth() {
+			return 10;
+		}
+
+		public override int GetMaxHeight() {
+			return 10;
+		}
+
 		public override void Paint(Level level) {
-			ShopNpc npc = null;
+			var d = Connected.Values.First();
+			
+			ShopNpc npc;
 
 			if (GlobalSave.IsFalse(ShopNpc.HatTrader)) {
 				npc = new HatTrader();
@@ -30,7 +44,44 @@ namespace BurningKnight.level.rooms.special {
 			}
 
 			level.Area.Add(npc);
-			npc.Center = GetTileCenter() * 16 + new Vector2(8);
+
+			var fl = Tiles.RandomFloorOrSpike();
+			
+			if (d.X == Left || d.Y == Right) {
+				var w = (int) (GetWidth() / 2f + Random.Int(-1, 1));
+				var door = new Vector2(Left + w, Random.Int(Top + 2, Bottom - 2));
+				
+				Painter.DrawLine(level, new Vector2(Left + w, Top), new Vector2(Left + w, Bottom), Tile.WallA);
+				Painter.Set(level, door, fl);
+				
+				npc.Center = new Vector2(Left + w + (d.X == Left ? 2 : -2), Random.Int(Top + 2, Bottom - 3)) * 16 + new Vector2(8);
+
+				var dr = new VerticalDoor();
+				dr.Center = door * 16 + new Vector2(16, 0);
+				level.Area.Add(dr);
+				
+				var v = (d.X == Left ? -1 : 1);
+				
+				Painter.DrawLine(level, new Vector2(Left + w + v, Top), new Vector2(Left + w + v, Bottom), Tile.WallA);
+				Painter.Set(level, door + new Vector2(v, 0), fl);
+			} else if (true) {
+				var h = (int) (GetHeight() / 2f + Random.Int(-1, 1));
+				var door = new Vector2(Random.Int(Left + 2, Right - 2), Top + h);
+				
+				Painter.DrawLine(level, new Vector2(Left, Top + h), new Vector2(Right, Top + h), Tiles.Pick(Tile.Chasm, Tile.Lava, Tile.SensingSpikeTmp));
+				Painter.Set(level, door, fl);
+				
+				npc.Center = new Vector2(Random.Int(Left + 2, Right - 2), Top + h + (d.Y == Top ? 2 : -2)) * 16 + new Vector2(8);
+				
+				var dr = new Door();
+				dr.Center = door * 16 + new Vector2(8, 8);
+				level.Area.Add(dr);
+
+				var v = (d.Y == Top ? -1 : 1);
+				
+				Painter.DrawLine(level, new Vector2(Left + 1, Top + h + v), new Vector2(Right - 1, Top + h + v), Tiles.Pick(Tile.Chasm, Tile.Lava, Tile.SensingSpikeTmp));
+				Painter.Set(level, door + new Vector2(0, v), fl);
+			}
 		}
 
 		public static bool ShouldBeAdded() {
