9a066bad7cf5f1ae9ed2982fd25a9744509cdff1
Egor Dorichev <egordorichev@gmail.com>
2019-09-26 18:09:21 +0300

    More test

--- Ctrings/token/Token.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Xna.Framework;
-
-namespace Ctrings.token {
-	public abstract class Token {
-		public string Text;
-		public abstract Vector2 Render(CFont font, Vector2 start, Ctring ctring);
-	}
-}
