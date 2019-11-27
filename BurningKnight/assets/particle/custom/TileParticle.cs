fb97fcda2a6957c3f39df35b511fefb5b140ba68
Egor Dorichev <egordorichev@gmail.com>
2019-11-27 18:02:33 +0300

    Tile particles now support going up and handle chasms

--- BurningKnight/assets/particle/custom/TileParticle.cs
+++ BurningKnight/assets/particle/custom/TileParticle.cs
@@ -15,11 +15,16 @@ namespace BurningKnight.assets.particle.custom {
 		public static float MaxZ = Display.Height;
 		
 		public TextureRegion Top;
+		public TextureRegion TopTarget;
 		public TextureRegion Side;
 		public TextureRegion Sides;
 		public Vector2 Scale = new Vector2(0, 3);
 		public float Z;
 		public Tile Tile;
+		public Tile OriginalTile;
+		public bool FromBottom;
+		public Vector2 Target;
+		public float TargetZ;
 		
 		public override void AddComponents() {
 			base.AddComponents();
@@ -27,26 +32,80 @@ namespace BurningKnight.assets.particle.custom {
 			Height = 24;
 			
 			AlwaysActive = true;
-			Z = MaxZ;
-			Depth = Layers.FlyingMob;
-			
-			Tween.To(1f, Scale.X, x => Scale.X = x, 0.4f);
-			Tween.To(1f, Scale.Y, x => Scale.Y = x, 0.4f);
 
-			Tween.To(0, Z, x => {
+			if (FromBottom) {
+				var level = Run.Level;
+				var x = (int) (X / 16);
+				var y = (int) ((Y + 8) / 16);
+
+				OriginalTile = level.Get(x, y);
+
+				if (OriginalTile == Tile.Chasm) {
+					Done = true;
+					return;
+				}
+				
+				if (!OriginalTile.IsWall()) {
+					Z = -8;
+				}
+				
+				Scale.X = 1;
+				Scale.Y = 1;
+				
+				level.Set(x, y, Tile.Chasm);
+				level.UpdateTile(x, y);
+				level.ReCreateBodyChunk(x, y);
+				
+				Tween.To(MaxZ, Z, z => {
+					if (z > 8) {
+						Depth = Layers.FlyingMob;
+					}
+
+					Z = z;
+				}, 0.5f, Ease.QuadIn).OnEnd = () => {
+					Tween.To(Target.X, X, v => X = v, 1f, Ease.QuadInOut);
+					Tween.To(Target.Y, Y, v => Y = v, 1f, Ease.QuadInOut).OnEnd = () => {
+						x = (int) ((Target.X + 8) / 16);
+						y = (int) ((Target.Y + 8) / 16);
+
+						if (level.Get(x, y) != Tile.Chasm) {
+							Top = TopTarget;
+						} else {
+							TargetZ = -8;
+							Tile = OriginalTile;
+						}
+
+						AnimateFall();
+					};
+				};
+			} else {
+				Z = MaxZ;
+				Depth = Layers.FlyingMob;
+
+				Tween.To(1f, Scale.X, x => Scale.X = x, 0.4f);
+				Tween.To(1f, Scale.Y, x => Scale.Y = x, 0.4f);
+
+				AnimateFall();
+			}
+
+			AddComponent(new ShadowComponent(RenderShadow));
+		}
+
+		private void AnimateFall() {
+			Tween.To(TargetZ, Z, x => {
 				Z = x;
 
 				if (Z <= 8f) {
 					Depth = 0;
 				}
 			}, 0.5f, Ease.QuadIn).OnEnd = () => {
-				Scale.X = 2;
-				Scale.Y = 0.5f;
+				Scale.X = 3;
+				Scale.Y = 0.3f;
 
 				Tween.To(1, Scale.X, x => Scale.X = x, 0.5f);
 				Tween.To(1, Scale.Y, x => Scale.Y = x, 0.5f).OnEnd = () => {
 					var level = Run.Level;
-					var x = (int) (X / 16);
+					var x = (int) (CenterX / 16);
 					var y = (int) ((Y + 8) / 16);
 					
 					level.Set(x, y, Tile);
@@ -56,8 +115,6 @@ namespace BurningKnight.assets.particle.custom {
 					Done = true;
 				};
 			};
-			
-			AddComponent(new ShadowComponent(RenderShadow));
 		}
 
 		public void RenderShadow() {
@@ -69,8 +126,11 @@ namespace BurningKnight.assets.particle.custom {
 			var v = Position + originB - new Vector2(0, Z - 16);
 			
 			Graphics.Render(Top, Position + origin - new Vector2(0, Z), 0, origin, Scale);
-			Graphics.Render(Side, v, 0, originB, Scale);
-			Graphics.Render(Sides, v, 0, originB, Scale);
+
+			if (Z >= 0) {
+				Graphics.Render(Side, v, 0, originB, Scale);
+				Graphics.Render(Sides, v, 0, originB, Scale);
+			}
 		}
 	}
 }
