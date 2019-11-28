43f66177a4c9910441803a6bd9dacad7254f9c3f
Egor Dorichev <egordorichev@gmail.com>
2019-11-28 18:59:43 +0300

    BK not starting boss fight fix?

--- BurningKnight/entity/creature/bk/BurningKnight.cs
+++ BurningKnight/entity/creature/bk/BurningKnight.cs
@@ -30,15 +30,15 @@ namespace BurningKnight.entity.creature.bk {
 		private BossPatternSet<BurningKnight> set;
 		private static Color tint = new Color(234, 50, 60, 200);
 		private Boss captured;
-		
+
 		public bool Hidden => GetComponent<StateComponent>().StateInstance is HiddenState;
-		
+
 		public override void AddComponents() {
 			base.AddComponents();
-			
+
 			AddTag(Tags.BurningKnight);
 			AddTag(Tags.PlayerSave);
-			
+
 			RemoveTag(Tags.Boss);
 			RemoveTag(Tags.LevelSave);
 			RemoveTag(Tags.MustBeKilled);
@@ -56,26 +56,26 @@ namespace BurningKnight.entity.creature.bk {
 			var b = new RectBodyComponent(6, 8, 10, 15, BodyType.Dynamic, true) {
 				KnockbackModifier = 0
 			};
-			
+
 			AddComponent(b);
 			b.Body.LinearDamping = 3;
-			
+
 			AddComponent(new BkGraphicsComponent("old_burning_knight"));
 			AddComponent(new LightComponent(this, 64f, new Color(1f, 0.7f, 0.2f, 1f)));
 
 			var health = GetComponent<HealthComponent>();
 			health.Unhittable = true;
-			
+
 			GetComponent<StateComponent>().Become<IdleState>();
 			AddComponent(new OrbitGiverComponent());
-			
+
 			AddComponent(new LightComponent(this, 32, new Color(1f, 0.2f, 0.1f, 0.5f)));
 
 			var buffs = GetComponent<BuffsComponent>();
-			
+
 			buffs.AddImmunity<FrozenBuff>();
 			buffs.AddImmunity<BurningBuff>();
-			
+
 			Subscribe<RoomChangedEvent>();
 			Subscribe<ItemTakenEvent>();
 			Subscribe<Dialog.EndedEvent>();
@@ -84,7 +84,7 @@ namespace BurningKnight.entity.creature.bk {
 			Subscribe<DiedEvent>();
 			Subscribe<SecretRoomFoundEvent>();
 			Subscribe<Boss.DefeatedEvent>();
-			
+
 			GetComponent<DialogComponent>().Dialog.Voice = 25;
 		}
 
@@ -92,10 +92,10 @@ namespace BurningKnight.entity.creature.bk {
 			if (Hidden) {
 				return;
 			}
-			
+
 			if (!Awoken && target != null) {
 				Awoken = true;
-				Become<FollowState>();		
+				Become<FollowState>();
 			} else if (target == null) {
 				Become<IdleState>();
 				Awoken = false;
@@ -103,21 +103,22 @@ namespace BurningKnight.entity.creature.bk {
 
 			base.OnTargetChange(target);
 		}
-		
+
 		private void FreeSelf() {
 			if (!Hidden) {
 				return;
 			}
-			
+
 			var graphics = GetComponent<BkGraphicsComponent>();
 			graphics.Alpha = 0;
 
 			Center = captured.Center;
 			GetComponent<HealthComponent>().Unhittable = true;
-				
+
 			Tween.To(1, graphics.Alpha, x => graphics.Alpha = x, 0.3f).OnEnd = () => {
 				Timer.Add(() => {
 					Become<ChaseState>();
+
 					// YOU CAN'T DEFEAT THE BURNING KNIGHT!!!
 					GetComponent<DialogComponent>().StartAndClose("bk_2", 5);
 				}, 1f);
@@ -132,15 +133,15 @@ namespace BurningKnight.entity.creature.bk {
 
 				return false;
 			}
-			
+
 			if (Hidden) {
 				return base.HandleEvent(e);
 			}
-		
+
 			if (e is RoomChangedEvent rce) {
 				var p = rce.Who is Player;
 				var bs = rce.Who is BurningKnight;
-				
+
 				if ((p || bs) && rce.New != null) {
 					var t = rce.New.Type;
 
@@ -179,15 +180,11 @@ namespace BurningKnight.entity.creature.bk {
 			} else if (e is Dialog.EndedEvent dse) {
 				if (dse.Owner is ShopKeeper && dse.Dialog.Id == "shopkeeper_18") {
 					// What a joke
-					Timer.Add(() => {
-						GetComponent<DialogComponent>().StartAndClose("bk_4", 5);
-					}, 1);
+					Timer.Add(() => { GetComponent<DialogComponent>().StartAndClose("bk_4", 5); }, 1);
 				}
 			} else if (e is ShopNpc.SavedEvent) {
 				// I WOULDN'T BOTHER EVEN TALKING TO THEM
-				Timer.Add(() => {
-					GetComponent<DialogComponent>().StartAndClose("bk_5", 5);
-				}, 2f);
+				Timer.Add(() => { GetComponent<DialogComponent>().StartAndClose("bk_5", 5); }, 2f);
 			} else if (e is ShopKeeper.EnragedEvent skee) {
 				if (skee.ShopKeeper.GetComponent<RoomComponent>().Room.Explored) {
 					// KILL HIM, EDWARD!
@@ -198,18 +195,18 @@ namespace BurningKnight.entity.creature.bk {
 					// EDWARD, NOOOOOO!
 					GetComponent<DialogComponent>().StartAndClose("bk_7", 5);
 				}
-				
+
 				return false;
 			} else if (e is SecretRoomFoundEvent) {
 				// OH COMON, STOP EXPLODING MY CASTLE!
 				GetComponent<DialogComponent>().StartAndClose("bk_8", 5);
 			}
-			
+
 			return base.HandleEvent(e);
 		}
 
 		private float lastFadingParticle;
-		
+
 		public override void Update(float dt) {
 			base.Update(dt);
 
@@ -218,7 +215,7 @@ namespace BurningKnight.entity.creature.bk {
 			}
 
 			lastFadingParticle -= dt;
-			
+
 			if (lastFadingParticle <= 0) {
 				lastFadingParticle = 0.2f;
 
@@ -232,15 +229,20 @@ namespace BurningKnight.entity.creature.bk {
 
 				if (room != null && room.Type == RoomType.Boss) {
 					CheckCapture();
+				} else if (Target != null) {
+					room = Target.GetComponent<RoomComponent>().Room;
+
+					if (room != null && room.Type == RoomType.Boss) {
+						CheckCapture();
+					}
 				}
 			}
 		}
-		
+
 		#region Buring Knight States while calm
 		public class IdleState : SmartState<BurningKnight> {
-			
 		}
-		
+
 		public class FollowState : SmartState<BurningKnight> {
 			public override void Update(float dt) {
 				base.Update(dt);
@@ -258,18 +260,24 @@ namespace BurningKnight.entity.creature.bk {
 
 				var room = Self.Target.GetComponent<RoomComponent>().Room;
 
-				if (Self.OnScreen && room != null && room.Type == RoomType.Regular && room.Tagged[Tags.MustBeKilled].Count > 0 && room.Contains(Self, 16f)) {
+				if (Self.OnScreen && room != null && room.Type == RoomType.Regular &&
+				    room.Tagged[Tags.MustBeKilled].Count > 0 && room.Contains(Self, 16f)) {
 					var aa = Self.AngleTo(room);
 					force = 400f * dt;
-					Self.GetComponent<RectBodyComponent>().Velocity -= new Vector2((float) Math.Cos(aa) * force, (float) Math.Sin(aa) * force);
+
+					Self.GetComponent<RectBodyComponent>().Velocity -=
+						new Vector2((float) Math.Cos(aa) * force, (float) Math.Sin(aa) * force);
+
 					return;
 				}
-				
+
 				var a = Self.AngleTo(Self.Target);
-				Self.GetComponent<RectBodyComponent>().Velocity += new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
+
+				Self.GetComponent<RectBodyComponent>().Velocity +=
+					new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
 			}
 		}
-		
+
 		public class FlyAwayState : SmartState<BurningKnight> {
 			public override void Update(float dt) {
 				base.Update(dt);
@@ -279,11 +287,14 @@ namespace BurningKnight.entity.creature.bk {
 
 				if (d > 80f) {
 					Self.Become<FollowState>();
+
 					return;
 				}
-				
+
 				var a = Self.AngleTo(Self.Target);
-				Self.GetComponent<RectBodyComponent>().Velocity += new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
+
+				Self.GetComponent<RectBodyComponent>().Velocity +=
+					new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
 			}
 		}
 
@@ -313,7 +324,7 @@ namespace BurningKnight.entity.creature.bk {
 					if (Self.Target != null) {
 						Self.Center = Self.Target.Center + MathUtils.CreateVector(Rnd.AnglePI(), 64f);
 					}
-					
+
 					Tween.To(1, graphics.Alpha, x => graphics.Alpha = x, 0.3f).OnEnd = () => {
 						if (Self.Target != null) {
 							Self.Become<FollowState>();
@@ -330,7 +341,7 @@ namespace BurningKnight.entity.creature.bk {
 			}
 		}
 		#endregion
-		
+
 		#region Burning Knight States while chasing
 		public class FlyAwayAttackingState : SmartState<BurningKnight> {
 			public override void Update(float dt) {
@@ -341,18 +352,21 @@ namespace BurningKnight.entity.creature.bk {
 
 				if (d > 96f) {
 					Self.Become<ChaseState>();
+
 					return;
 				}
-				
+
 				var a = Self.AngleTo(Self.Target);
-				Self.GetComponent<RectBodyComponent>().Velocity += new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
+
+				Self.GetComponent<RectBodyComponent>().Velocity +=
+					new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
 			}
 		}
-		
+
 		public class ChaseState : SmartState<BurningKnight> {
 			public override void Update(float dt) {
 				base.Update(dt);
-				
+
 				var d = Self.DistanceTo(Self.Target);
 				var force = 300f * dt;
 
@@ -364,15 +378,21 @@ namespace BurningKnight.entity.creature.bk {
 
 				var room = Self.Target.GetComponent<RoomComponent>().Room;
 
-				if (Self.OnScreen && room != null && room.Type == RoomType.Regular && room.Tagged[Tags.MustBeKilled].Count > 0 && room.Contains(Self, 16f)) {
+				if (Self.OnScreen && room != null && room.Type == RoomType.Regular &&
+				    room.Tagged[Tags.MustBeKilled].Count > 0 && room.Contains(Self, 16f)) {
 					var aa = Self.AngleTo(room);
 					force = 400f * dt;
-					Self.GetComponent<RectBodyComponent>().Velocity -= new Vector2((float) Math.Cos(aa) * force, (float) Math.Sin(aa) * force);
+
+					Self.GetComponent<RectBodyComponent>().Velocity -=
+						new Vector2((float) Math.Cos(aa) * force, (float) Math.Sin(aa) * force);
+
 					return;
 				}
-				
+
 				var a = Self.AngleTo(Self.Target);
-				Self.GetComponent<RectBodyComponent>().Velocity += new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
+
+				Self.GetComponent<RectBodyComponent>().Velocity +=
+					new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
 			}
 		}
 
@@ -391,17 +411,17 @@ namespace BurningKnight.entity.creature.bk {
 					p.Spectral = true;
 					p.Center = Self.Center;
 					p.Depth = Self.Depth;
-					
+
 					Become<ChaseState>();
 				}
 			}
 		}
-		
+
 		public override void SelectAttack() {
 			if (set == null) {
 				set = BurningKnightAttackRegistry.PatternSetRegistry.Generate(Run.Level.Biome.Id);
 			}
-			
+
 			base.SelectAttack();
 			GetComponent<StateComponent>().PushState(BurningKnightAttackRegistry.GetNext(set));
 		}
@@ -410,35 +430,35 @@ namespace BurningKnight.entity.creature.bk {
 		public class CaptureState : SmartState<BurningKnight> {
 			public override void Init() {
 				base.Init();
-				
+
 				Camera.Instance.Targets.Clear();
 				Camera.Instance.Follow(Self, 0.3f);
-				
-				Timer.Add(() => {
-					Camera.Instance.Follow(Self.captured, 0.3f);
-				}, 0.5f);
+
+				Timer.Add(() => { Camera.Instance.Follow(Self.captured, 0.3f); }, 0.5f);
 			}
 
 			public override void Update(float dt) {
 				base.Update(dt);
-				
+
 				var d = Self.DistanceTo(Self.captured);
-				
+
 				if (d <= 8) {
 					Audio.PlayMusic("Fatiga", true);
-					
+
 					// PREPARE TO DIE!
 					Self.captured.GetComponent<DialogComponent>().StartAndClose("bk_3", 5);
 					Self.GetComponent<AudioEmitterComponent>().EmitRandomized("mob_bk_capture");
 					Camera.Instance.Unfollow(Self);
-					
+
 					Become<HiddenState>();
 					Self.captured.SelectAttack();
-				} else if (d >= 300f && Self.GetComponent<RoomComponent>().Room != Self.captured.GetComponent<RoomComponent>().Room) {
+				} else if (d >= 300f &&
+				           Self.GetComponent<RoomComponent>().Room != Self.captured.GetComponent<RoomComponent>().Room) {
 					Become<TeleportState>();
+
 					return;
 				}
-				
+
 				var a = Self.AngleTo(Self.captured);
 				var force = 300f * dt;
 
@@ -446,19 +466,20 @@ namespace BurningKnight.entity.creature.bk {
 					force *= 2;
 				}
 
-				Self.GetComponent<RectBodyComponent>().Velocity += new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
+				Self.GetComponent<RectBodyComponent>().Velocity +=
+					new Vector2((float) Math.Cos(a) * force, (float) Math.Sin(a) * force);
 			}
 		}
-		
+
 		public class HiddenState : SmartState<BurningKnight> {
 			private bool teleported;
-			
+
 			public override void Init() {
 				base.Init();
 
 				Camera.Instance.Shake(20);
 				Self.Position = Vector2.Zero;
-				
+
 				Timer.Add(() => {
 					((InGameState) Engine.Instance.State).ResetFollowing();
 					Camera.Instance.Shake(10);
