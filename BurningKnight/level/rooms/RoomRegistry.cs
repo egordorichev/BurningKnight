e6001293a33fc96832342e76f9f34abff19368e7
Egor Dorichev <egordorichev@gmail.com>
2019-12-30 19:31:14 +0300

    Preparing for not so sopo

--- BurningKnight/level/rooms/RoomRegistry.cs
+++ BurningKnight/level/rooms/RoomRegistry.cs
@@ -141,7 +141,7 @@ namespace BurningKnight.level.rooms {
 				RoomInfo.New<DuckRoom>(1f),
 				RoomInfo.New<NurseRoom>(1f),
 				RoomInfo.New<ElonRoom>(1f),
-				RoomInfo.New<GobettaShopRoom>(1f + 1000f),
+				// RoomInfo.New<GobettaShopRoom>(1f + 1000f),
 				
 				// Boss
 				RoomInfo.New<BossRoom>(1f),
