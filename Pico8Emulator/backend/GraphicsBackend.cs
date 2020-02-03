34932a96a86abb0fdee8f0d8c507bb0e7b2b8d4d
Egor Dorichev <egordorichev@gmail.com>
2020-02-03 20:17:06 +0300

    Cut out pico8 emulation for now

--- Pico8Emulator/backend/GraphicsBackend.cs
+++
@@ -1,8 +0,0 @@
-namespace Pico8Emulator.backend {
-	public abstract class GraphicsBackend {
-		public Emulator Emulator;
-
-		public abstract void CreateSurface();
-		public abstract void Flip();
-	}
-}
