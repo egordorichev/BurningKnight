13eb768e4a83fe217db235c63e5b8d77f3e3b861
Egor Dorichev <egordorichev@gmail.com>
2020-09-23 18:01:21 +0300

    Removed all the deleted files

--- Pico8Emulator/unit/audio/ChannelData.cs
+++
@@ -1,6 +0,0 @@
-namespace Pico8Emulator.unit.audio {
-	public struct ChannelData {
-		public bool isSilent;
-		public byte sfxIndex;
-	}
-}
--- Pico8Emulator/unit/audio/PatternData.cs
+++
@@ -1,8 +0,0 @@
-namespace Pico8Emulator.unit.audio {
-	public struct PatternData {
-		public ChannelData[] channelCount;
-		public bool loopStart;
-		public bool loopEnd;
-		public bool shouldStop;
-	}
-}
